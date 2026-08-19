06fac2f21d0b8ebca11043f591546e30a03b92e9
felipeozacarias <felipe.ozacarias@gmail.com>
2026-08-19 18:55:35 -0300

    fix: move global Cypress media settings to root config

--- cypress.config.js
+++ cypress.config.js
@@ -9,34 +9,4 @@ async function setupNodeEvents(on, config) {
   on(
     'file:preprocessor',
     createBundler({
-      plugins: [createEsbuildPlugin(config)],
-    }),
-  );
-
-  return config;
-}
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'https://automationexercise.com',
-    specPattern: 'cypress/e2e/**/*.feature',
-    supportFile: 'cypress/support/e2e.js',
-    setupNodeEvents,
-    viewportWidth: 1366,
-    viewportHeight: 768,
-    defaultCommandTimeout: 12000,
-    requestTimeout: 15000,
-    responseTimeout: 30000,
-    video: true,
-    screenshotOnRunFailure: true,
-    chromeWebSecurity: false,
-    retries: {
-      runMode: 1,
-      openMode: 0
-    },
-    env: {
-      trelloActionUrl: 'https://api.trello.com/1/actions/592f11060f95a3d3d46a987a',
-      searchProduct: 'dress'
-    }
-  }
-});
+      plugins: [createEs
